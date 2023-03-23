e0c6b17469c7e2f060647cdf90f346d884442fcd
bwguan <bwguan@freewheel.tv>
2023-03-23 17:57:01 +0800

    part1 1.1-1.5

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7
+            },
+            {
+                name: 'State of a component',
+                exercises: 14
+            }
+        ]
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
 }
 
-export default App;
+const Header = (props) => {
+    const {course} = props;
+    return (
+        <h1>
+            {course}
+        </h1>
+    )
+}
+
+const Content = (props) => {
+    const {parts} = props;
+    return (
+        <div>
+            <p>
+                {parts[0].name} {parts[0].exercises}
+            </p>
+            <p>
+                {parts[1].name} {parts[1].exercises}
+            </p>
+            <p>
+                {parts[2].name} {parts[2].exercises}
+            </p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    const {parts} = props;
+    let total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+    return (
+        <p>
+            Number of exercises {total}
+        </p>
+    )
+}
+
+export default App
