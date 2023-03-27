60b2b17705e697c4800760b336c65c70d2941576
bwguan <bwguan@freewheel.tv>
2023-03-27 18:02:27 +0800

    part2-1

---
+++ part2/course/src/App.js
@@ -0,0 +1,98 @@
+import './App.css';
+
+const App = () => {
+    const courses = [
+        {
+            name: 'Half Stack application development',
+            id: 1,
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            name: 'Node.js',
+            id: 2,
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
+
+    return (
+        <div>
+            {
+                courses.map(course => <Course course={course}/>)
+            }
+        </div>
+    )
+}
+
+const Course = ({course}) => {
+    return (
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
+            <Total parts={course.parts}/>
+        </div>
+    )
+}
+
+const Header = (props) => {
+    const {course} = props;
+    return (
+        <h1>
+            {course}
+        </h1>
+    )
+}
+
+const Content = (props) => {
+    const {parts} = props;
+    return (
+        <div>
+            {
+                parts.map(part => <p>{part.name} {part.exercises}</p>)
+            }
+        </div>
+    )
+}
+
+const Total = (props) => {
+    const {parts} = props;
+    let total = parts.reduce((sum, p) => sum + p.exercises, 0)
+    return (
+        <p>
+            Total of exercises {total}
+        </p>
+    )
+}
+
+export default App
